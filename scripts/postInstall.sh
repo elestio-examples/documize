b600e00207617e6c4dd63702878e912210246a42
jbenguira <z51biz@gmail.com>
2022-08-18 17:14:22 +0300

    displaying curl result

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -8,7 +8,7 @@ META=$(curl -s http://$app_target |  grep -P -i "dbhash" | sed -e 's/^[[:space:]
 DBHASH=$(echo $META | sed 's/<meta property="dbhash" content="//g' | sed "s/\" \/>//g")
 
 app_target=$(docker-compose port app 5001)
-curl --output /dev/null --header "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
+curl --header "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
  --request POST --data-raw 'dbname=documize&dbhash='"$DBHASH"'&title=Documize&message=Documize+Community+instance+contains+all+our+documentation&allowAnonymousAccess=false&firstname=admin&lastname=admin&email='"$ADMIN_EMAIL"'&password='"$ADMIN_PASSWORD"'&activationKey=&edition=Community' \
  http://$app_target/api/setup
 
