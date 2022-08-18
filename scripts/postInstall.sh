2fbf6de758bb08327381535f662a0b308c2421a8
jbenguira <z51biz@gmail.com>
2022-08-18 17:17:38 +0300

    fixed postInstall

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,10 +4,11 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for Documize ot be ready"
 sleep 10s;
 
+app_target=$(docker-compose port app 5001)
+
 META=$(curl -s http://$app_target |  grep -P -i "dbhash" | sed -e 's/^[[:space:]]*//')
 DBHASH=$(echo $META | sed 's/<meta property="dbhash" content="//g' | sed "s/\" \/>//g")
 
-app_target=$(docker-compose port app 5001)
 curl --header "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
  --request POST --data-raw 'dbname=documize&dbhash='"$DBHASH"'&title=Documize&message=Documize+Community+instance+contains+all+our+documentation&allowAnonymousAccess=false&firstname=admin&lastname=admin&email='"$ADMIN_EMAIL"'&password='"$ADMIN_PASSWORD"'&activationKey=&edition=Community' \
  http://$app_target/api/setup
