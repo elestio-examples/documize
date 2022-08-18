32758a1d32ee5b971a9de0ee59b253b0ada4704e
jbenguira <z51biz@gmail.com>
2022-08-18 17:09:32 +0300

    fixed auto setup

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,8 +3,11 @@ set -o allexport; source .env; set +o allexport;
 
 sleep 10s;
 
+META=$(curl -s http://$app_target |  grep -P -i "dbhash" | sed -e 's/^[[:space:]]*//')
+DBHASH=$(echo $META | sed 's/<meta property="dbhash" content="//g' | sed "s/\" \/>//g")
+
 app_target=$(docker-compose port app 5001)
 curl --output /dev/null --header "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
- --request POST --data-raw 'dbname=documize&dbhash=61385e89a7&title=Documize&message=Documize+Community+instance+contains+all+our+documentation&allowAnonymousAccess=false&firstname=admin&lastname=admin&email='"$ADMIN_EMAIL"'&password='"$ADMIN_PASSWORD"'&activationKey=&edition=Community' \
+ --request POST --data-raw 'dbname=documize&dbhash='"$DBHASH"'&title=Documize&message=Documize+Community+instance+contains+all+our+documentation&allowAnonymousAccess=false&firstname=admin&lastname=admin&email='"$ADMIN_EMAIL"'&password='"$ADMIN_PASSWORD"'&activationKey=&edition=Community' \
  http://$app_target/api/setup
 
